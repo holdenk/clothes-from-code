a7f42eb1fefe1fffe945ace863d30b5ab37d77c2
Holden Karau <holden@pigscanfly.ca>
2019-04-21 00:01:00 -0700

    Install firefox into the container as needed for selenium, allow slightly longer dress names, fix driver login

--- dowork.sh
+++ dowork.sh
@@ -4,5 +4,7 @@ mkdir /tmp/"${DRESS_DIR}"
 # Download the file but not too big
 (ulimit -f 2024; wget --show-progress "${DRESS_CODE_URL}" -P /tmp/"${DRESS_DIR}"/ --max-redirect 0)
 INPUT_FILENAME=$(ls -1 /tmp/"${DRESS_DIR}"/)
+echo "Generating images"
 python gen.py --files /tmp/"${DRESS_DIR}"/"${INPUT_FILENAME}" --out /tmp/"${DRESS_DIR}"
+echo "Starting upload of images"
 python cowcow_uploader.py --dress_name "${DRESS_NAME}" --dress_dir /tmp/"${DRESS_DIR}"
