85556fd105fed85cd46a1bf72c1f12a5877b3070
Holden Karau <holden@pigscanfly.ca>
2019-04-21 00:00:26 -0700

    Use headless mode inside the container for selenium

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 cd /usr/src/app/
-MOZ_HEADLESS=1 FLASK_APP=/usr/src/app/server.py flask run --host=0.0.0.0
+MOZ_HEADLESS=1
+export MOZ_HEADLESS
+FLASK_APP=/usr/src/app/server.py flask run --host=0.0.0.0
