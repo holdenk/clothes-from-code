34314f7aa0fa8f7b4be79184c985e2a5433a7544
Holden Karau <holden@pigscanfly.ca>
2019-04-20 18:53:25 -0700

    Add geckodriver to the serving image and handle non-raw links

--- dockerfile
+++ dockerfile
@@ -1,8 +1,17 @@
 # base image
-FROM python:3.7.2-slim
+FROM python:3.7.2-stretch
 
 # set working directory
 WORKDIR /usr/src/app
+
+# Downoad flask
+RUN apt-get update && apt-get install python-flask3
+
+# Download gecko driver
+RUN wget https://github.com/mozilla/geckodriver/releases/download/geckodriver-v0.24.0-linux64.tar.gz && \
+    tar -xvf geckodriver-v0.24.0-linux64.tar.gz && \
+    mv geckodriver /bin/ && \
+    rm geckodriver-v0.24.0-linux64.tar.gz
 
 # add and install requirements
 COPY ./requirements.txt /usr/src/app/requirements.txt
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, Response, request, send_from_directory
+import re
 app = Flask(__name__)
 
 @app.route('/')
@@ -20,12 +21,43 @@ def stream_template(template_name, **context):
     rv.enable_buffering(5)
     return rv
 
+bad_regex = re.compile("^http(s|)://(www.|)github.com/(.*?/.*?)/blob/(.*)$", re.IGNORECASE)
+def handle_non_raw_code_urls(code_url):
+    """Some people will give us links to the non raw view"""
+    match = bad_regex.match(code_url)
+    if match is None:
+        return code_url
+    else:
+        return "https://raw.githubusercontent.com/{0}/{1}".format(
+            match.group(3), match.group(4))
+
+gh_raw_re = re.compile("^https://raw.githubusercontent.com/(.*?)/(.*?)/.*/(.*?)$")
+file_domain_re = re.compile("^https://(.*?)/.*/(.*?)$")
+def extract_dress_name(code_url):
+    """Try and turn a URL into a dress name"""
+    match = gh_raw_re.match(code_url)
+    if match is None:
+        match = file_domain_re.match(code_url)
+        if match is None:
+            return re.sub("^.*//.*/", " ", code_url)
+        else:
+            return match.group(1) + "'s " + match.group(2) + " glitch code dress"
+    else:
+        return match.group(1) + " " + match.group(2) + "'s " + match.group(3) + " glitch code dress" 
+    
 
 @app.route('/generate_dress', methods=["POST"])
 def generate_dress():
-    if request.form["url"] is None:
+    if request.form["url"] is None or len(request.form["url"]) == 0:
         return send_from_directory("static", "missing_url.html")
     else:
-        code_url = request.form["url"]
-        return Response(stream_template('generated.html', rows=rows))
+        requested_code_url = request.form["url"]
+        code_url = handle_non_raw_code_urls(requested_code_url)
+        dress_name = extract_dress_name(code_url)
+        rows = []
+        return Response(
+            stream_template('generated.html',
+                            dress_name=dress_name,
+                            code_url=code_url,
+                            rows=rows))
 
