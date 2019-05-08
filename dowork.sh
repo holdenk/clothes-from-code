110e7b9c8249160988381246e3ca8312de189656
Holden Karau <holden@pigscanfly.ca>
2019-05-08 15:19:27 -0700

    Unintentional revert

--- dowork.sh
+++ dowork.sh
@@ -5,6 +5,6 @@ mkdir /tmp/"${DRESS_DIR}"
 (ulimit -f 2024; wget --show-progress "${DRESS_CODE_URL}" -P /tmp/"${DRESS_DIR}"/ --max-redirect 0)
 INPUT_FILENAME=$(ls -1 /tmp/"${DRESS_DIR}"/)
 echo "Generating images"
-python gen.py --files /tmp/"${DRESS_DIR}"/"${INPUT_FILENAME}" --out /tmp/"${DRESS_DIR}"
+python gen.py --files /tmp/"${DRESS_DIR}"/"${INPUT_FILENAME}" --out /tmp/"${DRESS_DIR}" --clothing "${CLOTHING_TYPE}"
 echo "Starting upload of images"
 python cowcow_uploader.py --dress_name "${DRESS_NAME}" --dress_dir /tmp/"${DRESS_DIR}" --clothing "${CLOTHING_TYPE}"
